f3bca1230d00a1ad4f7299afd099edf8f1ff5c90
YumYumaCom <yumyuma.com@gmail.com>
2025-06-21 08:46:38 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="font-nunito bg-gray-100 text-lg min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-center">Welcome to YumYuma!</h1>
+    </div>
+  );
+}
